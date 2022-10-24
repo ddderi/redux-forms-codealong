d339377423bfe43c010715a40f83cff29af37155
ddderi <maxime830@live.fr>
2022-10-24 21:11:47 +1000

    done

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { addTodo } from ""
 
 class CreateTodo extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.dispatch({type: "enculado", payload: this.state})
+    this.setState({
+      text: ''
+    })
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target
+
+    this.setState({
+      text: value
+    });
+    console.log(this.state)
+  };
+
+
+
   render() {
-    return(
+
+    // const [todos] = this.props
+
+
+
+    return (
       <div>
-        Create Todo Component
+        {console.log(this.props.todos)}
+        <form onSubmit={this.handleSubmit} >
+          <input onChange={this.handleChange} 
+          value={this.state.text}
+          type="text" 
+          placeholder="add todo" />
+          <input type="submit" />
+        </form>
+        <ul>
+        {this.props.todos.length>0 ? this.props.todos.map((todo, index) => {return <li key={index}>{todo.text}</li>})  : <h1>Nothing to do</h1>}
+        </ul>
       </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = (dispatch) => {
+    return {
+      addTodo: (formData) => dispatch({type: "ADD_TODO", payload: formData})
+    }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  }
+}
+
+
+export default connect(mapStateToProps)(CreateTodo);
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -4,5 +4,13 @@ export default function manageTodo(state = {
 
   console.log("reducer received this action:", action);
 
+  switch(action.type){
+    case "enculado":
+      return {
+        todos: [...state.todos, action.payload]
+      }
+      default:
+        return state
+  }
   return state;
 }
